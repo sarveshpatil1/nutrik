852f9eafc4d2f66157d7961970debcabbeea2a6e
sarvesh patil <sarveshpatil0000@gmail.com>
2021-06-08 23:44:53 +0530

    bmi final version

--- app/src/main/java/com/example/nutri1/Nutiknow.java
+++ app/src/main/java/com/example/nutri1/Nutiknow.java
@@ -9,6 +9,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -40,6 +41,10 @@ public class Nutiknow extends AppCompatActivity  {
         );
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
